c3d968641cc52ff44d61ce993b455ce08ac76dd1
longclaw <hosein.latifi@outlook.com>
2016-09-23 23:52:42 +0330

    FinalTweaks

--- 1st/app.js
+++ 1st/app.js
@@ -3,25 +3,39 @@
 
 angular.module("LunchCheck", [])
 
-.controller("LunchCheckController", ["$scope"], function($scope){
+.controller("LunchCheckController", ["$scope", function($scope){
 
 $scope.message = "";
 $scope.items = "";
+$scope.color = "red";
 
 $scope.countItems = function(){
   if ($scope.items != "") {
+
     var result = $scope.items.split(',');
-    if (result.length <= 3) {
+    var counter = 0;
+
+    for (var i = 0; i < result.length; i++) {
+      if (result[i].trim()!="") {
+        counter++;
+      }
+    }
+
+    if (counter <= 3) {
       $scope.message = "Enjoy!";
     }else {
       $scope.message = "Too much!";
     }
+
+    $scope.color = "green";
+
   }else {
     $scope.message = "Please enter data first.";
+    $scope.color = "red";
   }
 }
 
 
-});
+}]);
 
 })();
